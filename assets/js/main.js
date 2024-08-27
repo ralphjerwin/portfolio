7be8ffb79694b3b62699c11d76eb4923e8c064c6
Your Name <ralphjerwin.dev@gmail.com>
2024-08-27 09:04:04 +0800

    fix header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,24 @@ import AOS from 'aos';
 AOS.init();  
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(event) {
     const hamburger = document.querySelector('.hamburger');
-    const navList = document.querySelector('.nav-list');
-
+    const navList = document.querySelector('.mobile-nav-list');
+    const listItems = document.querySelectorAll('.list-hide');
+    const Close = document.querySelector('.close');
+    const Bar = document.querySelector('bar');
+    
+    event.preventDefault();
     hamburger.addEventListener('click', function() {
         navList.classList.toggle('active');
+        Bar.style.display ='none';
+        Close.style.display ='block';
+    });
+    listItems.forEach(item => {
+        item.addEventListener('click', function() {
+            // Hide the navigation list when any link is clicked
+            navList.style.display = 'none';
+        });
     });
 });
 
